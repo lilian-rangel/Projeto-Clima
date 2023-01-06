c353d9d68d142c83a82fadea80c61f058d2006e9
lilian-rangel <lilianrm.nutri@gmail.com>
2023-01-06 10:39:26 -0300

    :construction: feat: change the data dinamically from API

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,10 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
+const weatherContainer = document.querySelector("#weather-data");
 
 // functions
 const getWeatherData = async(city) => {
@@ -22,14 +23,24 @@ const getWeatherData = async(city) => {
     const res = await fetch(apiWeatherUrl);
     const data = await res.json();
 
-    console.log(data);
+    return data;
 
 };
 
 
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
 
 };
 
@@ -41,4 +52,13 @@ searchBtn.addEventListener("click", (e) => {
     const city = cityInput.value;
 
     showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+
+    if(e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
 });
